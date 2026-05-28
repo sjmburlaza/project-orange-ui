27b9be7ff1153ed646c2836c7ab7696bcd70bd1c
sjmburlaza <hellosharmane@gmail.com>
2026-05-28 17:05:03 +0800

    Implement sidebar

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { CtaComponent } from 'src/app/features/common/cta/cta.component';
+import { OrderSummaryComponent } from 'src/app/features/common/order-summary/order-summary.component';
+import { VoucherComponent } from 'src/app/features/common/voucher/voucher.component';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [VoucherComponent, OrderSummaryComponent, CtaComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
+  private readonly router = inject(Router);
 
+  isCartRoute(): boolean {
+    return this.router.url.includes('/cart');
+  }
 }
